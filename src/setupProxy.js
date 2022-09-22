16a994b85449649f71c406a6077d31a06f9b4eb1
DI LU <77090688+TLzzs@users.noreply.github.com>
2022-09-22 14:19:49 +1000

    proxxy middleware

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,9 +1,9 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-module.exports = function(app) {
-   app.use('/users/**',
-    createProxyMiddleware({
-        target: 'http://localhost:9000',
-        changeOrigin: true
-    }));
-//   app.use(createProxyMiddleware('/otherApi/**', { target: 'http://localhost:5000' }));
- };
+//const { createProxyMiddleware } = require('http-proxy-middleware');
+//module.exports = function(app) {
+//   app.use('/users/**',
+//    createProxyMiddleware({
+//        target: 'http://localhost:9000',
+//        changeOrigin: true
+//    }));
+////   app.use(createProxyMiddleware('/otherApi/**', { target: 'http://localhost:5000' }));
+// };

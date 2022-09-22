5e021b87cb1cc651838d5b287dc60a8f9a230e78
troublepear <xiaohuyaw@gmail.com>
2022-09-22 21:53:33 +1000

    modal.js & css: pop-up window fix icon, image floating&resize, add responsive design

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,18 +1,21 @@
 import "../css/modal.css";
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
+import flowerBasket from "../image/pages/modalPlants.png";
+import item1 from "../image/items/item1.png";
 
 export default function Modal({close}){
     return(
-        <div className="main-container">
+        <div className="layout-model">
             <div className="modal-container">
-                <button className="button-close" onClick={()=>close()}>X</button>
+                <HighlightOffIcon onClick={()=>close()}/>
                 <div className="text-container">
                     <h1>Haven't Sign up?</h1>
                     <p>Let's get you started!</p>
-                    <a href="/user/register" className="button">Sign-Up</a>
+                    <a href="/user/register" className="button">Sign Up</a>
                     <p>Already have an account?</p>
-                    <a href="/user/login" className="button">Log-In</a>
+                    <a href="/user/login" className="button">Log In</a>
                 </div>
-                <div className="image-container"/>
+                <img className="modal-flower-basket" src={flowerBasket} alt="flowerBasket"></img>
             </div>
         </div>
     )

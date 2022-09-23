2f3c7e824840937497618837456ebc0231c856ee
troublepear <xiaohuyaw@gmail.com>
2022-09-23 23:14:03 +1000

    landing: add itemList and card

---
+++ src/components/itemList.js
@@ -0,0 +1,63 @@
+import "../css/itemList.css";
+import item1 from "../image/items/item1.png";
+import item2 from "../image/items/item2.png";
+import item3 from "../image/items/item3.png";
+import item4 from "../image/items/item4.png";
+import item5 from "../image/items/item5.png";
+
+export default function ItemList() {
+  return (
+    <div className="wrapper">
+      <Card
+        img={item1}
+        title="Tie Up Boots"
+        description="Fall Favorite • Boots"
+        price="45.00"
+      />
+
+      <Card
+        img={item2}
+        title="Plush Sweater"
+        description="Sweater Season • Cozy"
+        price="29.95"
+      />
+      <Card
+        img={item3}
+        title="Slim-Fit Demin"
+        description="Demin • Verstile"
+        price="24.99"
+      />
+      <Card
+        img={item4}
+        title="White Blouse"
+        description="Blouse • Lacey"
+        price="19.95"
+      />
+      <Card
+        img={item5}
+        title="White Blouse"
+        description="Blouse • Lacey"
+        price="19.95"
+      />
+      <Card
+        img={item1}
+        title="White Blouse"
+        description="Blouse • Lacey"
+        price="19.95"
+      />
+    </div>
+  );
+}
+
+function Card(props) {
+  return (
+    <div className="card">
+      <img src={props.img} alt="item" className="card__img" />
+      <div className="card__body">
+        <h2 className="card__title">{props.title}</h2>
+        <p className="card__description">{props.description}</p>
+        <h3 className="card__price">{props.price}</h3>
+      </div>
+    </div>
+  );
+}

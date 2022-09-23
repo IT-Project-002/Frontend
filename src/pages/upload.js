92637e7c9c9652013422ac2442272ca39c8c1c18
Lily Li <yifan11@student.unimelb.edu.au>
2022-09-23 15:09:16 +1000

    pass form and images to backend

--- src/pages/upload.js
+++ src/pages/upload.js
@@ -1,7 +1,20 @@
 import "../css/upload.css";
 import Select from "react-select";
-import React, {useState} from "react";
-import CameraAltRoundedIcon from '@mui/icons-material/CameraAltRounded';
+import React, {useState, useRef} from "react";
+import AWS, { ConnectContactLens } from 'aws-sdk'
+
+const S3_BUCKET ='it-project-002';
+const REGION ='ap-southeast-2';
+
+AWS.config.update({
+    accessKeyId: 'AKIA3V2C4OGZ2UVFEEHG',
+    secretAccessKey: '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'
+})
+
+const s3 = new AWS.S3({
+    params: { Bucket: S3_BUCKET},
+    region: REGION,
+})
 
 export default function Upload() {
     const options = [
@@ -22,18 +35,33 @@ export default function Upload() {
 
     /* Image */
     const [selectedImages, setSelectedImages] = useState([]);
+    const [progress , setProgress] = useState(0);
+    const [selectedFile, setSelectedFile] = useState(null);
+    const files = [];
+    const [filesDict, setFileDict] = useState({})
 
-    const onSelectFile = (event) => {
-      const selectedFiles = event.target.files;
-      const selectedFilesArray = Array.from(selectedFiles);
-  
-      const imagesArray = selectedFilesArray.map((file) => {
-        return URL.createObjectURL(file);
-      });
-      // save the previous selected images
-      setSelectedImages((previousImages) => previousImages.concat(imagesArray));
-      // FOR BUG IN CHROME
-      event.target.value = "";
+    const formRef = useRef();
+
+    const onSelectFile = (e) => {
+        // const file = e.target.files[0];
+        // files.concat(file);
+        // form.append('file', file)
+          const selectedFiles = e.target.files;
+          const selectedFilesArray = Array.from(selectedFiles);
+          console.log(selectedFiles)
+    
+          const imagesArray = selectedFilesArray.map((file) => {
+            return URL.createObjectURL(file);
+          });
+          for (var i = 0; i < selectedFiles.length; i++) {
+            filesDict[imagesArray[i]] = e.target.files[i]
+          }
+          console.log(filesDict)
+          // save the previous selected images
+          setSelectedImages((previousImages) => previousImages.concat(imagesArray));
+          // FOR BUG IN CHROME
+          e.target.value = "";
+        // setSelectedFile(e.target.files[0]);
     };
 
     const deleteImage = (image) => {
@@ -44,21 +72,46 @@ export default function Upload() {
     const handleSubmit = (e) => {
         // prevent page being refresh
         e.preventDefault();
-        const itemInfo = {itemName, price, describtion, tags, selectedImages}
-        console.log(itemInfo)
+        console.log(formRef.current)
+        const data = new FormData(formRef.current);
+        data.append("tags", JSON.stringify(tags))
+        const filesArray= selectedImages.map((file) => {
+            return filesDict[file]
+          })
+        for (let i = 0; i < filesArray.length; i++) {
+            data.append(i, filesArray[i]);
+        }
+        console.log(filesArray)
+        fetch('http://localhost:9000/user/upload',{
+            headers : {
+                // 'Content-Type':'application/json',
+                'Access-Control-Allow-Origin': '*',
+            },
+            method: 'POST',
+            mode: 'cors',
+            body: data
+        })
+        .then(itemInfo => {
+            console.log('Success:', itemInfo);
+        })
     }
 
     return (
-        <div className="layout-upload">
-
+        <div className="main">
+            {/* <div className="preview-container">
+                <img src={sample} alt="sample"></img>
+                <img src={sample} alt="sample"></img>
+                <img src={sample} alt="sample"></img>
+            </div> */}
+            {/* Image uploader*/}
             <div className="upload-container">
                 <label>
-                    <CameraAltRoundedIcon className="upload-icon"/>
-                    <input className= "upload-input" type="file"  name="itemImages" multiple accept="image/*"  onChange={onSelectFile}/>
+                    <input className= "upload-input" type="file"  name="itemImages" multiple accept="image/*" onChange={onSelectFile}/>
                 </label>
-                <p>Maximum 3 photos</p>
-            </div>   
-
+                <p>Upload more photos</p>
+            </div>
+            
+            {/* Image preview*/}
             <div className="preview-container">
                 {selectedImages &&
                     selectedImages.map((image, index) => {
@@ -71,56 +124,41 @@ export default function Upload() {
                     })
                 }
             </div>
-
             <div className="fillin-container">
-                <form method='post' onSubmit = {handleSubmit}>
-                    <div className="fillin-input-container">
-                        <h2>Name your cute work?</h2>
-                        <input 
-                        type="text"
+                <form method='post' onSubmit = {handleSubmit} enctype="multipart/form-data" ref={formRef}>
+                    <h2>Name your cute work?</h2>
+                    <input name="itemName" type="text"
                         value={itemName}
                         onChange={(e) => setItemName(e.target.value)}
                         required
-                        />
-                    </div>
-                    <div className="fillin-input-container">
-                        <h2>Price your work?</h2>
-                        <input type="number"
-                            value={price}
-                            onChange={(e) => setPrice(e.target.value)}
-                            required
-                        />
-                    </div>
-                    <div className="fillin-input-container">
+                    />
+                    <h2>Price your work?</h2>
+                    <input name="price" type="number"
+                        value={price}
+                        onChange={(e) => setPrice(e.target.value)}
+                        required
+                    />                    
                     <h2>Can you precisely describe your work?</h2>
-                        <input type="text"
-                            value={describtion}
-                            onChange={(e) => setDescribtion(e.target.value)}
-                            required
-                        />
-                    </div>
-                    <div>
-                        <div>
-                            <h2>Tag your work with its category.</h2>
-                        </div>
-                        <div className="selector-container">
-                        <Select 
-                            isMulti
-                            placeholder="Tell us what you interested in…"
-                            options={options}
-                            onChange={(item) => setTags(item)}
-                            isClearable={true}
-                            isSearchable={true}
-                            isDisabled={false}
-                            isLoading={false}
-                            isRtl={false}
-                            closeMenuOnSelect={false}
-                        />
-                        </div>
-                    </div>
-                    <div className="profile-button-container">
-                        <button type='submit'>Save Changes</button> 
-                    </div>
+                    <input name="describtion" type="text"
+                        value={describtion}
+                        onChange={(e) => setDescribtion(e.target.value)}
+                        required
+                    />
+                    {/* tag selection */}
+                    <h2>Tag your work with its category.</h2>
+                    <Select name="tag" className="tag"
+                        isMulti
+                        placeholder="Tell us what you interested in…"
+                        options={options}
+                        onChange={(item) => setTags(item)}
+                        isClearable={true}
+                        isSearchable={true}
+                        isDisabled={false}
+                        isLoading={false}
+                        isRtl={false}
+                        closeMenuOnSelect={false}
+                    />  
+                    <button className="button" type='submit'>Save Changes</button>
                 </form>
             </div>
         </div>

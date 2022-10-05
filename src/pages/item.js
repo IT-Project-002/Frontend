458d27bee5a87a056e70deb450b91ace21312fa2
haijiang <haijiang@student.unimelb.edu.au>
2022-10-06 02:23:22 +0800

    itemdetail: fixed responsive css

--- src/pages/item.js
+++ src/pages/item.js
@@ -3,12 +3,18 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Slider from "../components/imageSlider";
 import CircularProgress from "@mui/material/CircularProgress";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 
 export default function Item() {
   const access_token = sessionStorage.getItem("access_token");
   const { itemId } = useParams();
   const [data, setData] = useState({});
   const [loading, setLoading] = useState(true);
+  const [isActive, setIsActive] = useState(false);
+
+  const toggleButton = () => {
+    setIsActive((current) => !current);
+  };
 
   useEffect(() => {
     fetch("/users/item/" + itemId, {
@@ -46,13 +52,25 @@ export default function Item() {
             <Slider image={data["prod_images"]} />
           </div>
           <div className="item-desc-container">
-            <h1 className="item-name">{data["prod_name"]}</h1>
+            <div className="item-tags">
+              {data["prod_tags"]?.map((tag) => (
+                <div className="tag" key={tag}>
+                  {tag.charAt(0).toUpperCase() + tag.slice(1)}
+                </div>
+              ))}
+            </div>
+            <div className="item-name-like">
+              <h1 className="item-name">{data["prod_name"]}</h1>
+              <FavoriteBorderIcon
+                onClick={toggleButton}
+                style={{
+                  backgroundColor: isActive ? "#f48b8b" : "",
+                }}
+              />
+            </div>
             <h1 className="item-price">AU${data["prod_price"]}</h1>
-            {data["prod_tags"]?.map((tag) => (
-              <div key={tag}> {tag}</div>
-            ))}
             <p className="item-desc">{data["prod_desc"]}</p>
-            <p className="item-status">{data["user_name"]}'s Marketplace</p>
+            <p className="item-owner">{data["user_name"]}'s Marketplace</p>
             <p className="item-contact">{data["user_email"]}</p>
           </div>
         </div>

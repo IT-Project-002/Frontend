edb61f14458878f64f419b34d26a9503bc5a3c9e
DI LU <77090688+TLzzs@users.noreply.github.com>
2022-10-05 21:38:11 +1100

    item detail api fetch

--- src/pages/item.js
+++ src/pages/item.js
@@ -1,11 +1,36 @@
 import "../css/item.css";
 import Slider from "../components/imageSlider";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 export default function Item() {
+const access_token = sessionStorage.getItem("access_token");
   const { itemId } = useParams();
-  console.log(itemId);
+  const [data, setData] = useState({});
+  useEffect(() => {
+    fetch("/users/item/"+itemId, {
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        Authorization: "Bearer " + access_token,
+      },
+      method: "GET",
+      mode: "cors",
+    })
+      .then((res) => {
+        // console.log({res});
+        if (res.status === 401) {
+          sessionStorage.removeItem("access_token");
+          window.location.href = window.location.origin + "/user/login";
+        } else {
+          return res.json();
+        }
+      })
+      .then((dat) => {
+         console.log(dat);
+         setData(dat);
+      });
+  }, [access_token]);
   return (
     <div className="layout-item">
       <div className="item-img">

769f51274f6c5c7f338ddc7168b3d9600aaef577
haijiang <haijiang@student.unimelb.edu.au>
2022-10-12 23:28:08 +0800

    added search feature

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -8,8 +8,38 @@ import React, { useEffect, useState } from "react";
 
 function Landing() {
   const token = sessionStorage.getItem("access_token");
-  const [data, setData] = useState({});
+  const [data, setData] = useState([]);
+  const [result, setResult] = useState({});
+  const [displaySearch, setDisplaySearch] = useState(false);
   const [showModal, setShowModal] = useState(false);
+  const [search, setSearch] = useState("");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    fetch("/users/search", {
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      method: "POST",
+      mode: "cors",
+      body: JSON.stringify({ search: search }),
+    })
+      .then((res) => {
+        // console.log({res});
+        return res.json();
+      })
+      .then((dat) => {
+        setResult(dat);
+        setDisplaySearch(true);
+        console.log(dat);
+      });
+  };
+
+  const handleClick = () => {
+    setSearch("");
+    setDisplaySearch("");
+  };
 
   /* appear after 30 seconds */
   useEffect(() => {
@@ -44,11 +74,29 @@ function Landing() {
 
   return (
     <div className="layout-landing">
+      {/* Search */}
+      <form onSubmit={onSubmit}>
+        <div className="search-wrapper">
+          <input
+            type="text"
+            name="text"
+            placeholder="  Search items..."
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <button type="submit" className="search-button">
+            Search
+          </button>
+          <button type="button" className="search-button" onClick={handleClick}>
+            Clear
+          </button>
+        </div>
+      </form>
       <div className="slider-container">
         <Gallery autoPlay={false} />
       </div>
       <div className="browse-gird-container">
-        <Cards data={data} />
+        <Cards data={displaySearch ? result : data} />
       </div>
       {!token && showModal && <Modal className="pop-up" close={Toggle} />}
     </div>

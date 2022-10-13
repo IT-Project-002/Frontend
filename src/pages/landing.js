140467dc13aa947b76145c8cf66ca28f6df015df
Lily Li <yifan11@student.unimelb.edu.au>
2022-10-13 16:44:47 +1100

    Revert "Merge pull request #45 from IT-Project-002/search_final"
    This reverts commit 0ab084a380b6bd078b0aac28f124742d833091a9, reversing
    changes made to d7819a054d955c9fb76f4ed109bb796e5a94f8ca.

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -5,44 +5,11 @@ import Modal from "../components/modal";
 import Gallery from "../components/carousel";
 import Cards from "../components/itemListA";
 import React, { useEffect, useState } from "react";
-import ClearIcon from "@mui/icons-material/Clear";
-import IconButton from "@mui/material/IconButton";
-import { TextField } from "@mui/material";
 
 function Landing() {
   const token = sessionStorage.getItem("access_token");
-  const [data, setData] = useState([]);
-  const [result, setResult] = useState({});
-  const [displaySearch, setDisplaySearch] = useState(false);
+  const [data, setData] = useState({});
   const [showModal, setShowModal] = useState(false);
-  const [search, setSearch] = useState("");
-
-  const onSubmit = (e) => {
-    e.preventDefault();
-    fetch("/users/search", {
-      headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
-      },
-      method: "POST",
-      mode: "cors",
-      body: JSON.stringify({ search: search }),
-    })
-      .then((res) => {
-        // console.log({res});
-        return res.json();
-      })
-      .then((dat) => {
-        setResult(dat);
-        setDisplaySearch(true);
-        console.log(dat);
-      });
-  };
-
-  const handleClick = () => {
-    setSearch("");
-    setDisplaySearch(false);
-  };
 
   /* appear after 30 seconds */
   useEffect(() => {
@@ -77,29 +44,11 @@ function Landing() {
 
   return (
     <div className="layout-landing">
-      {/* Search */}
-      <form onSubmit={onSubmit}>
-        <div className="search-wrapper">
-          <div className="input-wrapper">
-            <input
-              type="text"
-              name="text"
-              placeholder="  Search items..."
-              value={search}
-              onChange={(e) => setSearch(e.target.value)}
-            ></input>
-            {search ? <ClearIcon onClick={handleClick} /> : null}
-          </div>
-          <button type="submit" className="search-button">
-            Search
-          </button>
-        </div>
-      </form>
       <div className="slider-container">
         <Gallery autoPlay={false} />
       </div>
       <div className="browse-gird-container">
-        <Cards data={displaySearch ? result : data} />
+        <Cards data={data} />
       </div>
       {!token && showModal && <Modal className="pop-up" close={Toggle} />}
     </div>
